16c4d88ff7a256b79f7f8f27242185375603a6fe
Egarozz <68447250+Egarozz@users.noreply.github.com>
2023-05-20 18:10:12 -0500

    Add files via upload

---
+++ Prueba2.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+import time
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+
+url = "https://zenbus.net/publicapp/web/limasanisidromibus77868410?line=677150016&stop=672740001&itinerary=822910079"
+
+browser = webdriver.Chrome(ChromeDriverManager().install(),options=chrome_options)
+browser.get(url)
+
+
+
+while(True):
+    time.sleep(2)
+    html = browser.page_source
+    soup = BeautifulSoup(html, 'lxml')
+    a = soup.find(class_="no-vehicle-content")
+    if(a is not None):
+        print(a.text)
+
